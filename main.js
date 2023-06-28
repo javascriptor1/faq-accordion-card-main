4066e9d120572bab08fa057418cccb4e82382529
Mohammed Fakih <mhd.fakih@gmail.com>
2023-06-28 14:45:08 +0300

    almost done

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) =>
+  question.addEventListener("click", () => {
+    if (question.nextElementSibling.classList.contains("hidden")) {
+      question.nextElementSibling.classList.toggle("hidden");
+      question.firstElementChild.nextElementSibling.classList.toggle('rotate-180');
+      question.firstElementChild.classList.toggle('font-bold');
+    } else {
+      question.nextElementSibling.classList.add("hidden");
+      question.firstElementChild.nextElementSibling.classList.toggle('rotate-180');
+      question.firstElementChild.classList.toggle('font-bold');
+    }
+  })
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkDesaturatedBlue: "hsl(238, 29%, 16%)",
+        softRed: "hsl(14, 88%, 65%)",
+        veryDarkGrayishBlue: "hsl(237, 12%, 33%)",
+        darkGrayishBlue: "hsl(240, 6%, 50%)",
+        lightGrayishBlue: "hsl(240, 5%, 91%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
