93ee00597f825d3f158d4f67cc47cb70c5722da0
Mohammed Fakih <mhd.fakih@gmail.com>
2023-06-28 14:46:36 +0300

    prettier

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ questions.forEach((question) =>
   question.addEventListener("click", () => {
     if (question.nextElementSibling.classList.contains("hidden")) {
       question.nextElementSibling.classList.toggle("hidden");
-      question.firstElementChild.nextElementSibling.classList.toggle('rotate-180');
-      question.firstElementChild.classList.toggle('font-bold');
+      question.firstElementChild.nextElementSibling.classList.toggle(
+        "rotate-180"
+      );
+      question.firstElementChild.classList.toggle("font-bold");
     } else {
       question.nextElementSibling.classList.add("hidden");
-      question.firstElementChild.nextElementSibling.classList.toggle('rotate-180');
-      question.firstElementChild.classList.toggle('font-bold');
+      question.firstElementChild.nextElementSibling.classList.toggle(
+        "rotate-180"
+      );
+      question.firstElementChild.classList.toggle("font-bold");
     }
   })
 );
